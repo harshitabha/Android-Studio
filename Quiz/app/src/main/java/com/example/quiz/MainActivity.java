00f44665d3f34413c41d5d2119dab41bfe7b8903
Harshita Bhardwaj <honeyblue2604@gmail.com>
2021-09-28 20:15:58 -0700

    Functionality almost complete
    The submit and next questions buttons are working as expected, and the progress bar functions. Now we just need to add a home and complete screen.

--- Quiz/app/src/main/java/com/example/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String[] qList;
+    private int qNum; // index of the question we are on in the qList
     private ArrayList<ArrayList<Double>> ansChoices;
 
     //progress bar stuff
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean isSubmit;
     private TextView q;
     private Button[] choices;
+    private ArrayList<Boolean> choiceClicked = new ArrayList<Boolean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submit = findViewById(R.id.submit);
         isSubmit = true;
         q = findViewById(R.id.question);
-
-        // initialize the choice buttons
-        choices = new Button[4];
-
-        for(int i = 0; i < choices.length; i++) {
-            // cant just use this string in the find view by ide function because it doesn't accept a string
-            String btnID = "choice" + i;
-
-            //this part of code is to get a button by an id that we determine using the string above
-            int resourceID = getResources().getIdentifier(btnID, "id",
-                    getPackageName());
-            choices[i] = (Button) findViewById(resourceID);
-            choices[i].setBackgroundColor(Color.WHITE);
-            //what to do when the button is pressed
-            choices[i].setOnClickListener(this);
-        }
+        qNum = 0;
 
         // initialize questions and answers list
         qList = new String[10];
@@ -69,11 +56,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ansChoices.add(ques.answerChoices()); // add the ans choices for each question
         }
 
+        // initialize the choice buttons
+        choices = new Button[4];
+        choiceClicked = new ArrayList<Boolean>(); // shows which button is selected
+
+
+        for(int i = 0; i < choices.length; i++) {
+            // cant just use this string in the find view by ide function because it doesn't accept a string
+            String btnID = "choice" + i;
+
+            //this part of code is to get a button by an id that we determine using the string above
+            int resourceID = getResources().getIdentifier(btnID, "id",
+                    getPackageName());
+            choices[i] = (Button) findViewById(resourceID);
+            choices[i].setTextColor(Color.parseColor("#000000"));
+
+            //what to do when the button is pressed
+            choices[i].setOnClickListener(this);
+            choiceClicked.add(false); // initialize the choice clicked array
+        }
+        updateQs();
 
     }
 
     @Override
     public void onClick(View v) {
+        /* Start Button */
+
+
+        /* selecting an ans choice */
+        if(isSubmit) {
+            String choiceID = v.getResources().getResourceEntryName(v.getId()); // get the id of the clicked button
+            int choiceIndex = Integer.parseInt(choiceID.substring(choiceID.length() - 1)); // the index of the button in the list
+
+            // if an answer was already picked
+            if (choiceClicked.contains(true)) {
+                // reset the list
+                for (int f = 0; f < choiceClicked.size(); f++) {
+                    choiceClicked.set(f, false);
+                    choices[f].setBackgroundColor(Color.parseColor("#d4d4d4"));
+                }
+            }
+            choiceClicked.set(choiceIndex, true); // update the choice clicked array
+            choices[choiceIndex].setBackgroundColor(Color.parseColor("#9e9e9e")); // make the selected choice button darker
+        }
+
+        /* Onclick listener for the submit button */
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSubmit = !isSubmit; //switch the val of the submit button
+
+                //if the button is currently displaying next question (after check ans has been clicked)
+                if(!isSubmit) {
+                    //checkAns();
+                    submit.setText("Next Question"); // set the text of the button to the next thing
+                }
+                //if the button is currently displaying the check ans (after next q has been clicked)
+                else{
+                    submit.setText("Check Answer"); // set the text of the button to the next thing
+
+                    // update the progress bar
+                    currentProgress += 100 / qList.length;
+                    progressBar.setProgress(currentProgress);
+                    progressBar.setMax(100);
+
+                    // update the questions
+                    updateQs();
+
+                }
+            }
+        });
+    }
+
+    private void updateQs(){
+        q.setText(qList[qNum]); // update the question
+        for(int i = 0; i < choices.length; i++) {
+            // cant just use this string in the find view by ide function because it doesn't accept a string
+            String btnID = "choice" + i;
+
+            //this part of code is to get a button by an id that we determine using the string above
+            int resourceID = getResources().getIdentifier(btnID, "id",
+                    getPackageName());
+
+            choices[i].setBackgroundColor(Color.parseColor("#d4d4d4")); // reset the bg color for the buttons
+            //update the ans choices
+            Double c = ansChoices.get(qNum).get(i);
+            choices[i].setText(c.toString());
+
+            choiceClicked.set(i, false); // reset the button clicker pointer to false
+        }
+
+        qNum++;
 
     }
 }
